e0627a1f295969c4c44df7326aa4acf3a43e7100
Sana Rzgar <sanarizgar34@gmail.com>
2023-03-17 20:50:07 +0300

    create: card for projects

---
+++ src/Component/ProjectCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { data } from "../Data";
+function ProjectCard() {
+  const dark = useSelector((state) => state.theme.theme);
+  return (
+    <div className="grid grid-cols-4 gap-10 justify-between items-center ">
+      {data.map((project) => {
+        return (
+          <div
+            key={project.id}
+            className={` ${
+              dark
+                ? "bg-grayy "
+                : "bg-dark  shadow-darkgray hover:shadow-darkgray"
+            } + shadow-md rounded hover:shadow-xl hover`}
+          >
+            <div className="flex justify-center items-center mt-4">
+              <img
+                className="h-60 w-60 object-cover rounded-full"
+                alt="projects"
+                src={project.url}
+              />
+            </div>
+
+            <div className="p-4">
+              <div className="text-lg text-transparent bg-clip-text bg-gradient-to-r from-pinkk to-purplee font-bold">
+                {project.title}
+              </div>
+              <p className="text-sm">{project.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ProjectCard;
